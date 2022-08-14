64b88d9c9eedc9dfca574676ff170e485a216a45
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-14 10:40:31 +0600

    memory update done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-
-document.getElementById('fourGB').addEventListener('click', function () {
-    const memory = document.getElementById('memory-cost');
-    memory.innerText = 100;
+function priceUpdate(itemId, price) {
+    const memory = document.getElementById(itemId);
+    memory.innerText = price;
     const memoryCost = document.getElementById('memory-cost').innerText;
     const storageCost = document.getElementById('storage-cost').innerText;
     const deliveryCost = document.getElementById('delivery-cost').innerText;
     const totalCost = parseFloat(memoryCost) + parseFloat(storageCost) + parseFloat(deliveryCost);
     const totalCostOutput = document.getElementById('total-cost');
     totalCostOutput.innerText = totalCost;
+}
+document.getElementById('fourGB').addEventListener('click', function () {
+    priceUpdate('memory-cost', 100);
 })
+document.getElementById('eightGB').addEventListener('click', function () {
+    priceUpdate('memory-cost', 150)
+})
+document.getElementById('sixteenGB').addEventListener('click', function () {
+    priceUpdate('memory-cost', 200)
+})
+
+/* document.getElementById('eightGB').addEventListener('click', function () {
+    const memory = document.getElementById('memory-cost');
+    memory.innerText = 150;
+    const memoryCost = document.getElementById('memory-cost').innerText;
+    const storageCost = document.getElementById('storage-cost').innerText;
+    const deliveryCost = document.getElementById('delivery-cost').innerText;
+    const totalCost = parseFloat(memoryCost) + parseFloat(storageCost) + parseFloat(deliveryCost);
+    const totalCostOutput = document.getElementById('total-cost');
+    totalCostOutput.innerText = totalCost;
+}) */
+
